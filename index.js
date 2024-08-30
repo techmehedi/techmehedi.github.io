1ee8faf3687555b1c9a95a81b45ed164a3e1b291
techmehedi <ecommehedi@gmail.com>
2024-08-30 18:15:02 -0400

    Changes

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 
+
 let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape")
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
 
-  for (let i=0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0; 
-    const boolInt = isOdd ? -1: 1;
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
-  }
+    for ( let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
 }
 
-function toggleContrast(){
-  contrastToggle = !contrastToggle;
-  if(contrastToggle){
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
     document.body.classList += " dark-theme"
-  }
-  else {
-    document.body.classList.remove("dark-theme")
-  }
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
 }
 
 
@@ -45,21 +46,18 @@ function contact(event) {
         .catch(() => {
           loading.classList.remove("modal__overlay--visible");
           alert(
-            "The email service is temporarily unavailable. Please contact me directly on ecommehedi@email.com"
+            "The email service is temporarily unavailable. Please contact me directly on mehedihasan.ccny@gmail.com"
           );
         });
     }
 
   
     function toggleModal() {
-      if(isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
+      if (isModalOpen) {
+          isModalOpen = false;
+          return document.body.classList.remove("modal--open");
       }
-
       isModalOpen = true;
-
-     // toggle modal
       document.body.classList += " modal--open";
-        
-    }
+      
+  }
