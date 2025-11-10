92761e1d9dc24aa16b9be4559508d5efa924c20a
techmehedi <ecommehedi@gmail.com>
2025-11-09 22:48:27 -0500

    Add Next.js configuration with webpack HMR optimizations

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+    unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ['three', 'gsap'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      }
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
+
