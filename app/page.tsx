11bd3e19b77884015ad6485ed3d83d43f4eb71d1
techmehedi <ecommehedi@gmail.com>
2025-11-23 15:17:18 -0500

    Update page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Lenis as ReactLenis } from 'lenis/react'
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
@@ -11,9 +10,6 @@ import Skills from '@/components/Skills'
 import Activities from '@/components/Activities'
 import Footer from '@/components/Footer'
 import ContactModal from '@/components/ContactModal'
-import ParticleBackground from '@/components/ParticleBackground'
-import BackgroundEffects from '@/components/BackgroundEffects'
-import { initAnimations } from '@/lib/animations'
 import { initEmailJS } from '@/lib/emailjs'
 
 export default function Home() {
@@ -21,15 +17,12 @@ export default function Home() {
 
   useEffect(() => {
     initEmailJS()
-    initAnimations()
   }, [])
 
   return (
-    <ReactLenis root>
-      <main>
-        <ParticleBackground />
-        <BackgroundEffects />
-        <Navigation onContactClick={() => setIsModalOpen(true)} />
+    <>
+      <Navigation onContactClick={() => setIsModalOpen(true)} />
+      <main className="site-shell">
         <Hero onContactClick={() => setIsModalOpen(true)} />
         <Projects />
         <Education />
@@ -37,9 +30,9 @@ export default function Home() {
         <Skills />
         <Activities />
         <Footer onContactClick={() => setIsModalOpen(true)} />
-        <ContactModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       </main>
-    </ReactLenis>
+      <ContactModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   )
 }
 
